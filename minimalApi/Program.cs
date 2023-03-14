a9bd36a7c223fe672ce0d98a27214597b0baf4c4
YelllenR <aurelie@rosedarbon.com>
2023-03-14 14:03:52 +0100

    Creation of routes

--- minimalApi/Program.cs
+++ minimalApi/Program.cs
@@ -1,14 +1,16 @@
 //Construction of postgres sql connexion string to use in the DbContext
 
+using AutoMapper;
 using minimalApi.Data;
+using minimalApi.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>();
-builder.Services.AddScoped<ICommandRepo, CommandRepo>(); 
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); 
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -20,9 +22,31 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.Run();
+//Using dependancy injection on the request to get the repo
+app.MapGet(
+    "api/v1/commands",
+    async (ICommandRepo repo, IMapper mapper) =>
+    {
+        //Gets any collection in Db
+        var commands = await repo.GetAllCommands();
+        return Results.Ok(mapper.Map<IEnumerable<CommandReadDto>>(commands));
+    }
+);
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+//Model binding
+//int id can be changed to [FromRoute]int id = it will need import the Microsoft.AspNetCore.Mvc
+app.MapGet(
+    "api/v1/commands/{id}",
+    async (ICommandRepo repo, IMapper mapper, int id) =>
+    {
+        var command = await repo.GetCommandById(id);
+        //Check if commande !null
+        if (command is not null)
+        {
+            return Results.Ok(mapper.Map<CommandReadDto>(command));
+        }
+        return Results.NotFound();
+    }
+);
+
+app.Run();
